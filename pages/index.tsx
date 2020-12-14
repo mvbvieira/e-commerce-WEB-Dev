a1784cac1b849e2529e2a8f21b9202dbc27fa41a
Marcos Vieira <mvbvieira>
2020-12-14 20:58:53 -0300

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import React from 'react';
+
+const Home: React.FC = () => {
+  return <h1>Hello World</h1>
+}
+
+export default Home;
